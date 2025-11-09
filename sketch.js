e8a74f7f6a571a37afa9f09062e1200759a87d3e
j-ssrf <jasmin.soraruf@mail.polimi.it>
2025-11-09 13:28:43 +0100

    si iniziaaa

--- sketch.js
+++ sketch.js
@@ -1,17 +1,17 @@
+
+let table;
+
+
 function preload() {
-  // put preload code here
+  table = loadTable('FREEDOMHOUSE_ES3.csv', 'csv', 'header');
 }
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  createCanvas(windowWidth, 1250);
 }
 
+
 function draw() {
-  // put drawing code here
+  background(220);
 }
